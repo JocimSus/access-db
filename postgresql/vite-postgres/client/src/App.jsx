4b2ab1bf9c2dcfed24eb80c52bb554580359df6d
Joachim Susatiyo <joe.susatiyo@gmail.com>
2024-05-09 16:24:11 +0700

    successfully connect to backend: * Data not showing up

--- postgresql/vite-postgres/client/src/App.jsx
+++ postgresql/vite-postgres/client/src/App.jsx
@@ -1,15 +1,30 @@
-import React from "react"
+import { useEffect, useState } from "react"
 
 import Insert from "./Insert.jsx"
-
 import "./App.css"
 
 function App() {
+  const [backendData, setBackendData] = useState(null)
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setBackendData(data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <>
       <div className="title">Access Scores Database</div>
       <div className="input-block">
         <Insert />
+        {backendData == null ? (
+          <p>Loading...</p>
+        ) : (
+          backendData.users.map((user, i) => {
+            ;<p key={i}>{user}</p>
+          })
+        )}
       </div>
     </>
   )
