9fef15c2a1045bd2a5d7d2002e868391374e965c
Joachim Susatiyo <joe.susatiyo@gmail.com>
2024-05-11 15:26:21 +0700

    Backend: * Server uses api routes * Setup routes * Separate file to store queries * Setup controller file * TODO: updateStudent function needs to be more flexible

---
+++ postgresql/vite-postgres/server/src/students_data/routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const controller = require("./controller");
+
+const router = Router();
+
+router.get("/", controller.getStudents);
+router.post("/", controller.addStudent);
+router.get("/:nis", controller.getStudentsByNis);
+router.put("/:nis", controller.updateStudent);
+router.delete("/:nis", controller.deleteStudent);
+
+module.exports = router;
