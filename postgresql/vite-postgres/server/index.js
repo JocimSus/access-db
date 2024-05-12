d38bf41b7cbd6909dd134f44541eeb1952f2660b
Joachim Susatiyo <joe.susatiyo@gmail.com>
2024-05-12 20:58:15 +0700

    cors

--- postgresql/vite-postgres/server/index.js
+++ postgresql/vite-postgres/server/index.js
@@ -6,6 +6,16 @@ const port = 5000;
 
 app.use(express.json());
 
+const cors = require("cors");
+// Allow all origins
+app.use(cors());
+// Allow specific origin(s)
+app.use(
+  cors({
+    origin: "https://yourdeployedsite.com",
+  })
+);
+
 app.use("/api/v1/students_data", studentsDataRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
